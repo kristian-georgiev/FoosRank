32323931bdce5708fe00b49766e87fadb0e3744b
maddy <maddyz@mit.edu>
2018-10-28 17:46:24 -0400

    verified connected now

--- src/db.js
+++ src/db.js
@@ -12,5 +12,7 @@ const db = mongoose.connection;
 
 // db error handling
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+db.on('connected', function() {
+      console.log('database connected');
+});
 module.exports = db;
