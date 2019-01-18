efc769964067d15f7866dd0822fd9484961b3137
root <root@Kris-XPS.mit.edu>
2019-01-17 21:14:56 -0500

    add landing page

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,13 @@ var uid = null;
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
-            print(user)
+            user.providerData.forEach(function (profile) {
+                console.log("Sign-in provider: " + profile.providerId);
+                console.log("  Provider-specific UID: " + profile.uid);
+                console.log("  Name: " + profile.displayName);
+                console.log("  Email: " + profile.email);
+                console.log("  Photo URL: " + profile.photoURL);
+            });
         }else{
             // redirect to login page
             uid = null;
