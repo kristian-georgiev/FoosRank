39563d83bf88555c7e84f30fa382c50415fec982
root <krisgrg@mit.edu>
2019-01-19 18:12:44 -0500

    encapsulate .js file with login control

--- public/js/game.js
+++ public/js/game.js
@@ -1,96 +1,120 @@
-var yellow_sc = 0;
-var black_sc  = 0;
-var score_history = [];
-var last_to_score = null;
-
-var y_button = document.getElementById("yellow_score");
-var b_button = document.getElementById("black_score");
-
-var undo_button = document.getElementById("undo");
-
-var scoretable = document.getElementById("scoretable");
-
-// Scoring goals
-
-y_button.onclick = function() {
-    yellow_sc += 1;
-    scoretable.innerHTML = yellow_sc + " : " + black_sc;
-    score_history.push("y");
-    if(is_game_over()){
-        display_popup("Yellow")
-    }
-};
-
-b_button.onclick = function() {
-    black_sc += 1;
-    scoretable.innerHTML = yellow_sc + " : " + black_sc;
-    score_history.push("b");
-    if(is_game_over()){
-        display_popup("Black")
-    }
-};
-
-// Undoing actions
-
-undo_button.onclick = function() {
-    last_to_score = score_history.pop()
-    if (last_to_score == "y") {
-        yellow_sc -= 1;
-    } 
-    if (last_to_score == "b") {
-        black_sc -= 1;
-    }
-    scoretable.innerHTML = yellow_sc + " : " + black_sc;
-};
+var mainApp = {};
+var firebase = app_fireBase;
 
 
-// Checking status of game
+(function(){
+var uid = null 
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
 
-function is_game_over() {
-    if ((yellow_sc == 10 && black_sc <=8) || (yellow_sc > 10 && (yellow_sc - black_sc == 2)) ) {
-        return "y"
-    }
-    if ((black_sc == 10 && yellow_sc <=8) || (black_sc > 10 && (black_sc - yellow_sc == 2)) ) {
-        return "b"
-    }
-    return false
- };
+        var yellow_sc = 0;
+        var black_sc  = 0;
+        var score_history = [];
+        var last_to_score = null;
+
+        var y_button = document.getElementById("yellow_score");
+        var b_button = document.getElementById("black_score");
+
+        var undo_button = document.getElementById("undo");
+
+        var scoretable = document.getElementById("scoretable");
+
+        // Scoring goals
+
+        y_button.onclick = function() {
+            yellow_sc += 1;
+            scoretable.innerHTML = yellow_sc + " : " + black_sc;
+            score_history.push("y");
+            if(is_game_over()){
+                display_popup("Yellow")
+            }
+        };
+
+        b_button.onclick = function() {
+            black_sc += 1;
+            scoretable.innerHTML = yellow_sc + " : " + black_sc;
+            score_history.push("b");
+            if(is_game_over()){
+                display_popup("Black")
+            }
+        };
+
+        // Undoing actions
+
+        undo_button.onclick = function() {
+            last_to_score = score_history.pop()
+            if (last_to_score == "y") {
+                yellow_sc -= 1;
+            } 
+            if (last_to_score == "b") {
+                black_sc -= 1;
+            }
+            scoretable.innerHTML = yellow_sc + " : " + black_sc;
+        };
 
 
-// Popup upon ending game
+        // Checking status of game
 
-var popup = document.getElementById("popup");
-var popup_text = document.getElementById("popup_text");
-var popup_undo = document.getElementById("popup_undo");
-var popup_continue = document.getElementById("popup_continue")
-
-function display_popup(winning_team) {
-    popup_text.innerHTML = "<h1>" + winning_team + " wins!</h1>"
-    popup.style.display = "block";
-  };
-
-  popup_undo.onclick = function() {
-    last_to_score = score_history.pop()
-    if (last_to_score == "y") {
-        yellow_sc -= 1;
-    } 
-    if (last_to_score == "b") {
-        black_sc -= 1;
-    }
-    scoretable.innerHTML = yellow_sc + " : " + black_sc;
-    popup.style.display = "none";
-  };
-
-  popup_continue.onclick = function() {
-      record_game()
-      window.location = "game_setup.html"
-
-  };
+        function is_game_over() {
+            if ((yellow_sc == 10 && black_sc <=8) || (yellow_sc > 10 && (yellow_sc - black_sc == 2)) ) {
+                return "y"
+            }
+            if ((black_sc == 10 && yellow_sc <=8) || (black_sc > 10 && (black_sc - yellow_sc == 2)) ) {
+                return "b"
+            }
+            return false
+        };
 
 
-// Record game results
+        // Popup upon ending game
 
-function record_game(){
-    return false
-}
+        var popup = document.getElementById("popup");
+        var popup_text = document.getElementById("popup_text");
+        var popup_undo = document.getElementById("popup_undo");
+        var popup_continue = document.getElementById("popup_continue")
+
+        function display_popup(winning_team) {
+            popup_text.innerHTML = "<h1>" + winning_team + " wins!</h1>"
+            popup.style.display = "block";
+        };
+
+        popup_undo.onclick = function() {
+            last_to_score = score_history.pop()
+            if (last_to_score == "y") {
+                yellow_sc -= 1;
+            } 
+            if (last_to_score == "b") {
+                black_sc -= 1;
+            }
+            scoretable.innerHTML = yellow_sc + " : " + black_sc;
+            popup.style.display = "none";
+        };
+
+        popup_continue.onclick = function() {
+            record_game()
+            window.location = "waiting_area.html"
+
+        };
+
+
+        // Record game results
+
+        function record_game(){
+            return false
+        }
   
+
+}else{
+    // redirect to login page
+    uid = null;
+    window.location.replace("login.html");
+}
+});
+
+function logOut(){
+firebase.auth().signOut();
+}
+
+mainApp.logOut = logOut;
+})()
---
+++ public/js/waiting_area.js
@@ -0,0 +1,32 @@
+var mainApp = {};
+var firebase = app_fireBase;
+
+
+(function(){
+var uid = null // TODO: I don't think this is needed?    
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            user.providerData.forEach(function (profile) {
+                console.log("Sign-in provider: " + profile.providerId);
+                console.log("  Provider-specific UID: " + profile.uid);
+                console.log("  Name: " + profile.displayName);
+                console.log("  Email: " + profile.email);
+                console.log("  Photo URL: " + profile.photoURL);
+
+            });
+
+
+        }else{
+            // redirect to login page
+            uid = null;
+            window.location.replace("login.html");
+        }
+    });
+
+    function logOut(){
+        firebase.auth().signOut();
+    }
+
+    mainApp.logOut = logOut;
+})()
