7ebbf04d963725e2f1eb9d84acceac23e6e74620
root <krisgrg@mit.edu>
2019-01-26 21:52:02 -0500

    merge login and index

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,13 @@
             return true;
         },
         uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
             document.getElementById('loader').style.display = 'none';
         }
         },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
         signInSuccessUrl: 'main.html',
         signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID
             ],
         // Terms of service url.
